920041887da4aa803210393d7b1dda44908286e6
Anurag <110625540+Aprogrammer45@users.noreply.github.com>
2026-02-11 22:46:06 +0530

    string program added

---
+++ string.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<string>
+#include<unordered_map>
+using namespace std;
+string a,b,choice;
+int l,f;
+class intro{
+    public:
+    intro(){
+        cout<<"Experimenting With strings"<<endl;
+    }
+};
+class funct{
+    public:
+    void concat(string a,string b){
+                   cout<<a+b<<endl;       
+    }
+    void length(){
+    cout<<"String1 or String2(Enter No.)"<<endl;
+    int c;
+    cin>>c;
+    l=(c==1)?a.length():b.length();
+    cout<<l<<endl;
+    }
+};
+class process:public funct{
+    public:
+    unordered_map<string,int>f={{"Concat",1},{"Length",2}};
+    void proc(unordered_map<string,int>f){
+        if(f.find(choice)!=f.end()){
+        switch(f[choice]){
+            case 1:
+            concat(a,b);
+            break;
+            case 2:
+            length();
+        }
+    }
+    else{
+        cout<<"Invalid Input"<<endl;
+    }
+}
+};
+
+class store{
+    public:
+    void input(){
+        cout<<"Enter a random string1 = "<<endl;
+        cin>>a;
+        cout<<"Enter a random string2 = "<<endl;
+        cin>>b;
+        cout<<"Choose Parameter:\n Concat\n Length\n "<<endl;
+        cin>>choice;
+    
+    }
+};
+
+int main(){
+    intro i;
+    store s;
+    for(int i;i<3;i++){
+    s.input();
+    process p;
+    unordered_map<string,int>f={{"Concat",1},{"Length",2}};
+    p.proc(f);}
+    return 0;
+};
